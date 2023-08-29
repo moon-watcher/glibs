3321420d705530f1f63fd698ae28485c8a4feaf0
MoonWatcherMD <langares@yahoo.es>
2023-08-29 02:10:15 +0200

    Refactor libpsg

--- libpsg/libpsg.c
+++ libpsg/libpsg.c
@@ -3,26 +3,29 @@
 
 void libpsg_init(struct libpsg *const psg, const unsigned char *const data, unsigned char isPAL)
 {
-	psg->sn.data = data;
 	psg->isPAL = isPAL;
-	libpsg_pause(psg);
+	psg->sn.data = data;
+	psg->isPlaying = 0;
 }
 
 void libpsg_play(struct libpsg *const psg, unsigned char track)
 {
 	psg->vtimer = 0;
-	libpsg_resume(psg);
+	psg->isPlaying = 1;
 	SN76489_play(&psg->sn, track);
 }
 
 void libpsg_stop(struct libpsg *const psg)
 {
-	libpsg_pause(psg);
 	psg->sn.data = 0;
+	psg->isPlaying = 0;
 }
 
 void libpsg_update(struct libpsg *const psg)
 {
+	if (!psg->sn.data)
+		return;
+
 	if (psg->isPlaying && (psg->isPAL || (psg->vtimer % 6)))
 		SN76489_update(&psg->sn);
 
