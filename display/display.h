49499a05f4abe5f75008a5010d5f327a0b20f5e0
MoonWatcherMD <langares@yahoo.es>
2023-09-04 01:46:31 +0200

    Remove display_init()

--- display/display.h
+++ display/display.h
@@ -3,7 +3,6 @@
 #include "config/helper.h"
 #include GLIBS_DISPLAY_HELPER_FILE
 
-void display_init();
 void display_on(unsigned char);
 void display_off(unsigned char);
 void display_prepare(unsigned short *, unsigned char);
