8c6f258e9c8205e242bd038ce04886945943f279
langares@yahoo.es <langares@yahoo.es>
2024-05-17 11:51:13 +0200

    Revert "DISPLAY_PREPARE macros"
    This reverts commit 8a7012b40f8209e4cb75c17bfcc77704652a2b15.

--- implement/display/SGDK/helper.h
+++ implement/display/SGDK/helper.h
@@ -1,16 +1,16 @@
 #pragma once
 
-#define DISPLAY_PREPARE_IMAGE(X, PAL) \
-    display_prepare(((Image *)X)->palette->data, PAL)
+#define DISPLAY_IMAGE(X) \
+    ((Image *)X)->palette->data
 
-#define DISPLAY_PREPARE_MULTIFONT(X) \
-    display_prepare(((multifont *)X)->palette_data, ((multifont *)X)->pal)
+#define DISPLAY_MULTIFONT(X) \
+    ((multifont *)X)->palette_data, ((multifont *)X)->pal
 
-#define DISPLAY_PREPARE_SD(X, PAL) \
-    display_prepare(((SpriteDefinition *)X)->palette->data, PAL)
+#define DISPLAY_SD(X) \
+    ((SpriteDefinition *)X)->palette->data
 
-#define DISPLAY_PREPARE_PALETTE(X, PAL) \
-    display_prepare(((Palette *)X)->data, PAL)
+#define DISPLAY_PALETTE(X) \
+    ((Palette *)X)->data
 
-#define DISPLAY_PREPARE_SPRITE(X, PAL) \
-    display_prepare(((Sprite *)X)->definition->palette->data, PAL)
+#define DISPLAY_SPRITE(X) \
+    ((Sprite *)X)->definition->palette->data
