6aff57d643c03b7434d3db119edf5784b63ef43a
langares@yahoo.es <langares@yahoo.es>
2024-06-24 20:43:34 +0200

    Remove from display

--- display/SGDK/helper.h
+++ display/SGDK/helper.h
@@ -3,9 +3,6 @@
 #define DISPLAY_IMAGE(X) \
     ((Image *)X)->palette->data
 
-#define DISPLAY_MULTIFONT(X) \
-    ((multifont *)X)->palette_data, ((multifont *)X)->pal
-
 #define DISPLAY_SD(X) \
     ((SpriteDefinition *)X)->palette->data
 
