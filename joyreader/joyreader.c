615b85cb1044d4302eecef071756d0c90b2b4720
langares@yahoo.es <langares@yahoo.es>
2024-08-26 03:16:26 +0200

    JOYREADER_CHANGE

--- joyreader/joyreader.c
+++ joyreader/joyreader.c
@@ -1,17 +1,33 @@
 #include "joyreader.h"
 
-void joyreader_init(joyreader *const joy)
+void joyreader_update(unsigned *joy, unsigned value)
 {
-    joy->changed = joy->active = 0;
+    *joy = ((value ^ ((*joy >> 16) & 0xFFFF)) << 16) | value;
+
+    // joy->changed = active ^ joy->active;
+    // joy->active = active;
+
+    /*
+    x = 0b1010;  printf("x: %32b \n", x);
+    y = 200;     printf("y: %32b \n", y);
+
+    c = ( x << 16 ) | y;      printf("c: %32b \n", c);
+    x = ( c>>16 ) & 0xFFFF;   printf("x: %32b \n", x);
+    y = c & 0xFFFF;           printf("y: %32b \n", y);
+    */
+
+    //     *joy = ((active ^ ((*joy >> 16) & 0xFFFF)) << 16) | active;
+
+    // c = ( x << 16 ) | y;
+
+    // joy->changed = active ^ joy->active;
+    // joy->active = active;
+
+    // X: changed
+    // Y: active
 }
 
-void joyreader_update(joyreader *const joy, unsigned active)
+void joyreader_press(unsigned *joy, unsigned value)
 {
-    joy->changed = active ^ joy->active;
-    joy->active = active;
-}
-
-void joyreader_press(joyreader *const joy, unsigned value)
-{
-    joy->active = value;
+    // joy->active = value;
 }
--- joyreader/joyreader.h
+++ joyreader/joyreader.h
@@ -1,13 +1,14 @@
 #pragma once
 
-typedef struct
-{
-    unsigned changed;
-    unsigned active;
-} joyreader;
+void joyreader_update(unsigned *, unsigned);
+void joyreader_press(unsigned *, unsigned);
 
-void joyreader_init(joyreader *const);
-void joyreader_update(joyreader *const, unsigned);
-void joyreader_press(joyreader *const, unsigned);
+#define JOYREADER_UPDATE(J, V) ({auto __V = V; J = ((__V ^ ((J >> 16) & 0xFFFF)) << 16) | __V; J; })
+#define JOYREADER_PRESS(J, V)  
+#define JOYREADER_ACTIVE(J)    (J & 0xFFFF)
+#define JOYREADER_CHANGED(J)   ((J >> 16) & 0xFFFF)
+#define JOYREADER_INACTIVE(J)  (~JOYREADER_ACTIVE(J))
+#define JOYREADER_PRESSED(J)   (JOYREADER_ACTIVE(J) & JOYREADER_CHANGED(J))
+#define JOYREADER_RELEASED(J)  (JOYREADER_INACTIVE(J) & JOYREADER_CHANGED(J))
 
 #include "config.h"
