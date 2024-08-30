aac54ee00dd7ddf61565f8ea6f5f34ec23aea659
langares@yahoo.es <langares@yahoo.es>
2024-08-30 08:54:05 +0200

    joyreader

--- joyreader/joyreader.h
+++ joyreader/joyreader.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define JOYREADER_UPDATE(J, V) ({ J = *(unsigned *)&(struct { short changed, active; }){V ^ J, V}; })
-#define JOYREADER_ACTIVE(J)    ((short)J)
+#define JOYREADER_UPDATE(J, V) ({ J = *(unsigned int*)&(struct { unsigned short changed, active; }){V ^ J, V}; })
+#define JOYREADER_ACTIVE(J)    ((unsigned short)J)
 #define JOYREADER_CHANGED(J)   (J >> 16)
 #define JOYREADER_INACTIVE(J)  (~JOYREADER_ACTIVE(J))
 #define JOYREADER_PRESSED(J)   (JOYREADER_CHANGED(J) & JOYREADER_ACTIVE(J))
