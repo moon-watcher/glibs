256e3e81f87db697267f2d42dda90580ac84a997
langares@yahoo.es <langares@yahoo.es>
2024-09-01 03:13:50 +0200

    Joyreader

--- joyreader/joyreader.h
+++ joyreader/joyreader.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define JOYREADER_UPDATE(J, V) ({ J = *(unsigned int*)&(struct { unsigned short changed, active; }){V ^ J, V}; })
+#define JOYREADER_UPDATE(J, V) ({ J = *(unsigned int *)&(struct { unsigned short changed, active; }){V ^ J, V}; })
 #define JOYREADER_ACTIVE(J)    ((unsigned short)J)
 #define JOYREADER_CHANGED(J)   (J >> 16)
 #define JOYREADER_INACTIVE(J)  (~JOYREADER_ACTIVE(J))
@@ -8,3 +8,14 @@
 #define JOYREADER_RELEASED(J)  (JOYREADER_CHANGED(J) & JOYREADER_INACTIVE(J))
 
 #include "config.h"
+
+// void joyreader_update(joyreader *const joy, unsigned active)
+// {
+//     joy->changed = active ^ joy->active;
+//     joy->active = active;
+// }
+
+// void joyreader_press(joyreader *const joy, unsigned value)
+// {
+//     joy->active = value;
+// }
