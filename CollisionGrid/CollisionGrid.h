18974c36699873ca2cffdc3e2aace373f1bee9fa
langares@yahoo.es <langares@yahoo.es>
2025-02-07 12:06:54 +0100

    no message

--- CollisionGrid/CollisionGrid.h
+++ CollisionGrid/CollisionGrid.h
@@ -24,15 +24,15 @@ typedef struct
 
 //
 
-CollisionGrid      *cg_init(struct CG_RECT, unsigned, unsigned, unsigned);
-void                cg_reset(CollisionGrid *const);
-void                cg_end(CollisionGrid *);
+CollisionGrid *cg_init(struct CG_RECT, unsigned, unsigned, unsigned);
+void cg_reset(CollisionGrid *const);
+void cg_end(CollisionGrid *);
 
 struct CG_CELL *cg_CELL_get(CollisionGrid *const, unsigned, unsigned);
-void               *cg_CELL_addItem(struct CG_CELL *const, void *const);
-unsigned            cg_CELL_removeItem(struct CG_CELL *const, void *const);
+void *cg_CELL_addItem(struct CG_CELL *const, void *const);
+unsigned cg_CELL_removeItem(struct CG_CELL *const, void *const);
 
-unsigned            cg_RECT_get(CollisionGrid *const, struct CG_RECT *const, struct CG_CELL *[]);
-void                cg_RECT_addItem(struct CG_CELL *[], unsigned, void *const);
-unsigned            cg_RECT_getItems(struct CG_CELL *[], unsigned, void *[]);
-void                cg_RECT_removeItem(struct CG_CELL *[], unsigned, void *const);
+unsigned cg_RECT_get(CollisionGrid *const, struct CG_RECT *const, struct CG_CELL *[]);
+void cg_RECT_addItem(struct CG_CELL *[], unsigned, void *const);
+unsigned cg_RECT_getItems(struct CG_CELL *[], unsigned, void *[]);
+void cg_RECT_removeItem(struct CG_CELL *[], unsigned, void *const);
