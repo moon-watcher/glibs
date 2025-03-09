0efd8925f8e9fd65f247e740dbdaaa9ba49b4a44
langares@yahoo.es <langares@yahoo.es>
2025-03-09 04:58:52 +0100

    de_entity_resume() & ...pause() & safety

--- darkenLite/darken.c
+++ darkenLite/darken.c
@@ -1,21 +1,81 @@
 #include "darken.h"
 
+
 // State
 
-void *de_state_empty() { return de_state_empty; }
+void *de_state_empty()
+{
+    return de_state_empty;
+}
 
 
 // Entity
 
-inline void de_entity_set       (de_entity *const this, de_state state) { this->state      = state; }
-inline void de_entity_destructor(de_entity *const this, de_state state) { this->destructor = state; }
-inline void de_entity_delete    (de_entity *const this)                 { this->state      = 0; }
-inline void de_entity_exec      (de_entity *const this)                 { this->state(this->data, this); }
+inline de_entity *de_entity_set(de_entity *const this, de_state state)
+{
+    if (this)
+        this->state = state;
+
+    return this;
+}
+
+inline de_entity *de_entity_destructor(de_entity *const this, de_state state)
+{
+    if (this)
+        this->destructor = state;
+
+    return this;
+}
+
+inline de_entity *de_entity_delete(de_entity *const this)
+{
+    if (this)
+        this->state = 0;
+
+    return this;
+}
+
+inline de_entity *de_entity_pause(de_entity *const this)
+{
+    if (this && this->manager && dcalloc_remove(this->manager, this) > DCALLOC_NOT_FOUND)
+        return this;
+    
+    return 0;
+}
+
+inline de_entity *de_entity_resume (de_entity *const this)
+{
+    if (this && this->manager && dcalloc_restore(this->manager, this) > DCALLOC_NOT_FOUND)
+        return this;
+
+    return 0;
+}
+
+inline void *de_entity_exec(de_entity *const this)
+{
+    if (this && this->state)
+        return this->state(this->data, this);
+
+    return 0;
+}
 
 
 // Manager
 
-void de_manager_update(de_manager *const this)
+void de_manager_init(de_manager *const this, unsigned bytes)
+{
+    dcalloc_init(this, sizeof(de_entity) + bytes);
+}
+
+de_entity *de_manager_new(de_manager *const this, de_state state)
+{
+    de_entity *const entity = dcalloc_alloc(this);
+    entity->manager = this;
+
+    return de_entity_set(entity, state);
+}
+
+unsigned de_manager_update(de_manager *const this)
 {
     for (unsigned i = 0; i < this->size;)
     {
@@ -27,6 +87,28 @@ void de_manager_update(de_manager *const this)
             de_entity_set(entity, entity->destructor ?: de_state_empty);
         }
 
-        entity->state = entity->state(entity->data, entity);
+        entity->state = de_entity_exec(entity);
     }
+
+    return this->size;
+}
+
+int de_manager_reset(de_manager *const this)
+{
+    int ret = dcalloc_iterator(this, de_entity_delete);
+
+    if (ret > DCALLOC_NULL_ITERATOR)
+        de_manager_update(this);
+
+    return ret;
+}
+
+int de_manager_end(de_manager *const this)
+{
+    int ret = de_manager_reset(this);
+
+    if (ret > DCALLOC_NULL_ITERATOR)
+        dcalloc_end(this);
+
+    return ret;
 }
--- darkenLite/darken.h
+++ darkenLite/darken.h
@@ -1,10 +1,17 @@
 #pragma once
 
+
 // State
 
 typedef void *(*de_state)();
 
 void *de_state_empty();
+
+
+// Manager
+
+#include "../dcalloc/dcalloc.h"
+typedef dcalloc de_manager;
 
 
 // Entity
@@ -13,26 +20,23 @@ typedef struct de_entity
 {
     de_state state;
     de_state destructor;
+    de_manager *manager;
 
     unsigned char data[]; // Bytes for data
 } de_entity;
 
-void de_entity_set        (de_entity *const, de_state);
-void de_entity_destructor (de_entity *const, de_state);
-void de_entity_delete     (de_entity *const);
-void de_entity_exec       (de_entity *const);
+de_entity *de_entity_set        (de_entity *const, de_state);
+de_entity *de_entity_destructor (de_entity *const, de_state);
+de_entity *de_entity_delete     (de_entity *const);
+de_entity *de_entity_pause      (de_entity *const);
+de_entity *de_entity_resume     (de_entity *const);
+void      *de_entity_exec       (de_entity *const);
 
 
 // Manager
 
-#include "../dcalloc/dcalloc.h"
-typedef dcalloc de_manager;
-
-void de_manager_update(de_manager *const);
-
-#define de_manager_init(THIS, BYTES) dcalloc_init(THIS, sizeof(de_entity) + BYTES)
-#define de_manager_new(THIS, STATE)  ({void *const e = dcalloc_alloc(THIS); de_entity_set(e, STATE); e;})
-#define de_manager_pause_entity      dcalloc_remove
-#define de_manager_resume_entity     dcalloc_restore
-#define de_manager_reset(THIS)       dcalloc_iterator(THIS, de_entity_delete); de_manager_update(THIS)
-#define de_manager_end(THIS)         de_manager_reset(THIS); dcalloc_end(THIS)
+void       de_manager_init   (de_manager *const, unsigned);
+de_entity *de_manager_new    (de_manager *const, de_state);
+unsigned   de_manager_update (de_manager *const);
+int        de_manager_reset  (de_manager *const);
+int        de_manager_end    (de_manager *const);
