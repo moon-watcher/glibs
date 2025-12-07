8464147b0b5f62c42fd1ade4b41388ee2d89e2b9
Moon-watcher <langares@yahoo.es>
2025-12-07 03:53:13 +0100

    no message

--- frameloader/implement/SGDK.c
+++ frameloader/implement/SGDK.c
@@ -8,19 +8,18 @@ void frameloader_SGDK_updater(frameloader *$)
     TileSet *tileset = ((Animation *)$->resource)->frames[$->frame]->tileset;
 
     u16 len = tileset->numTile << 4;
-    void *from = FAR_SAFE(tileset->tiles, len << 1);
-    u16 to = $->vrampos << 5;
+    u32 from = (u32)FAR_SAFE(tileset->tiles, len << 1);
+    u32 to = $->vrampos << 5;
 
-    /** Copy of DMA_queueDma() */
-    u32 fromAddr = (u32)from;
-    u32 bankLimitB = 0x20000 - (fromAddr & 0x1FFFF);
-    u32 bankLimitW = bankLimitB >> 1;
+    /** Copy-esque of DMA_queueDma() */
+    u32 bankLimitW = (0x20000 - (from & 0x1FFFF)) >> 1;
 
     if (len > bankLimitW)
     {
-        DMA_queueDmaFast(DMA_VRAM, (void *)(fromAddr + bankLimitB), to + bankLimitB, len - bankLimitW, 2);
+        u32 bankLimitB = bankLimitW << 1;
+        DMA_queueDmaFast(DMA_VRAM, (void *)(from + bankLimitB), to + bankLimitB, len - bankLimitW, 2);
         len = bankLimitW;
     }
 
-    DMA_queueDmaFast(DMA_VRAM, from, to, len, 2);
+    DMA_queueDmaFast(DMA_VRAM, (void *)from, to, len, 2);
 }
