474c1c5fb04f1e9f7031f8b22651f10e482b66ad
Moon-watcher <langares@yahoo.es>
2025-12-23 17:33:05 +0100

    no message

--- bitflag.h
+++ bitflag.h
@@ -1,9 +1,8 @@
 #pragma once
 
-#define BITFLAG_INIT(obs)          ((obs) = 0u)
 #define BITFLAG_ADD(obs, flag)     ((obs) |= (flag))
-#define BITFLAG_CLEAR(obs, flag)   ((obs) &= ~(flag))
 #define BITFLAG_HAS(obs, flag)    (((obs) & (flag)) != 0u)
+#define BITFLAG_CLEAR(obs, flag)   ((obs) &= ~(flag))
 #define BITFLAG_TOGGLE(obs, flag)  ((obs) ^= (flag))
 
 #define BITFLAG(bit) (1 << (bit))
