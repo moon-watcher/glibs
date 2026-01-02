ca43b864b8ff90a3ac2afe2ef92cee4a78051873
Moon-watcher <langares@yahoo.es>
2026-01-02 02:45:22 +0100

    no message

--- bitflag.h
+++ bitflag.h
@@ -1,45 +1,21 @@
 #pragma once
 
-#include <stdint.h>
-
-inline uint32_t bitflag_set(uint32_t *obs, uint32_t flag)
+inline void bitflag_set(unsigned *obs, unsigned flag)
 {
-    return *obs |= (1U << flag);
+    *obs |= (1U << flag);
 }
 
-inline uint32_t bitflag_get(uint32_t *obs, uint32_t flag)
+inline unsigned bitflag_get(unsigned *obs, unsigned flag)
 {
     return *obs & (1U << flag);
 }
 
-inline uint32_t bitflag_clear(uint32_t *obs, uint32_t flag)
+inline void bitflag_clear(unsigned *obs, unsigned flag)
 {
-    return *obs &= ~(1U << flag);
+    *obs &= ~(1U << flag);
 }
 
-inline uint32_t bitflag_flip(uint32_t *obs, uint32_t flag)
+inline void bitflag_flip(unsigned *obs, unsigned flag)
 {
-    return *obs ^= (1U << flag);
+    *obs ^= (1U << flag);
 }
-
-inline uint32_t bitflag_mask(uint32_t flag)
-{
-    return (1U << flag);
-}
-
-
-/*
-uint32_t flags;
-
-enum { FLAG_1, FLAG_2, FLAG_3, };
-
-kprintf("bitflag_set FLAG_1 // 1 // %d", bitflag_set(&flags, FLAG_1));
-// kprintf("bitflag_set FLAG_2 // 2 // %d", bitflag_set(&flags, FLAG_2));
-kprintf("bitflag_set FLAG_3 // 4 // %d", bitflag_set(&flags, FLAG_3));
-
-kprintf("bitflag_mask FLAG_1 // 1 // %d", bitflag_mask(FLAG_1));
-kprintf("bitflag_mask FLAG_2 // 2 // %d", bitflag_mask(FLAG_2));
-kprintf("bitflag_mask FLAG_3 // 4 // %d", bitflag_mask(FLAG_3));
-
-kprintf("%d - %d %d %d ", flags, bitflag_get(&flags, FLAG_1), bitflag_get(&flags, FLAG_2), bitflag_get(&flags, FLAG_3));
-*/
