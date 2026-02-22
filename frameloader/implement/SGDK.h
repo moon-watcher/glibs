1c286782bff5f01eac4408f55e55582a223952d4
Moon-watcher <langares@yahoo.es>
2026-02-22 15:09:50 +0100

    no message

--- frameloader/implement/SGDK.h
+++ frameloader/implement/SGDK.h
@@ -6,4 +6,4 @@
 void frameloader_SGDK_updater(frameloader *const);
 
 #define FRAMELOADER_SGDK_SPRITEDEF(D, X) \
-    D.animations[X], D.animations[X]->numFrame
+    (D).animations[X], (D).animations[X]->numFrame
