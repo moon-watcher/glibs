1d4a31d4a5d92261e71c1886cd239cb401ca5118
Moon-watcher <->
2026-04-04 17:04:32 +0200

    no message

--- statem/statem.c
+++ statem/statem.c
@@ -1,71 +1,50 @@
 #include "statem.h"
 
-void statem_init(statem_t *$, struct statem_entry_t *entries)
+void *statem_init(statem_t *$, struct statem_entry_t *entries)
 {
     $->entries = entries;
-    $->current = entries[0];
-    $->index = 0;
-    $->timer = 0;
+    $->count = 0;
+
+    while (entries[$->count].ptr)
+        ++$->count;
+
+    return statem_goto($, 0);
 }
 
 uint16_t statem_tick(statem_t *$)
 {
-    return ++$->timer >= $->current.duration;
+    return ++$->timer >= $->duration;
 }
 
 void *statem_next(statem_t *$)
 {
-    struct statem_entry_t *next = $->entries + $->index + 1;
+    return statem_goto($, $->index + 1 < $->count ? $->index + 1 : 0);
+}
 
-    $->index = next->ptr ? ++$->index : 0;
-    $->current = $->entries[$->index];
+void *statem_prev(statem_t *$)
+{
+    return statem_goto($, ($->index ?: $->count) - 1);
+}
+
+void *statem_goto(statem_t *$, uint16_t index)
+{
+    if (!$->count || index >= $->count)
+        return 0;
+
+    $->ptr = $->entries[index].ptr;
+    $->duration = $->entries[index].duration;
+    $->index = index;
     $->timer = 0;
 
-    return $->current.ptr;
-}
-
-void statem_setDuration(statem_t *$, uint32_t duration)
-{
-    $->current.duration = duration;
-}
-
-uint32_t statem_getDuration(statem_t *$)
-{
-    return $->current.duration;
+    return $->ptr;
 }
 
 uint16_t statem_getProgress(statem_t *$)
 {
-    uint32_t d = $->current.duration;
-    return d ? ($->timer * 100) / d : 0;
+    return $->duration ? ($->timer * 100) / $->duration : 0;
 }
 
-// void *statem_reset(statem_t *$)
-// {
-//     $->current = $->entries;
-//     $->timer = 0;
-
-//     return $->current->ptr;
-// }
-
-// uint16_t statem_getIndex(statem_t *$)
-// {
-//     return $->current - $->entries;
-// }
-
-// void *statem_goto(statem_t *$, uint16_t index)
-// {
-//     $->current = $->entries + index;
-//     $->timer = 0;
-
-//     return $->current->state;
-// }
-
-// void *statem_prev(statem_t *$)
-// {
-//     uint16_t index = $->current == $->entries
-//                          ? $->count - 1
-//                          : $->current - $->entries - 1;
-
-//     return statem_goto($, index);
-// }
+void *statem_reset(statem_t *$)
+{
+    return statem_goto($, 0);
+}
--- statem/statem.h
+++ statem/statem.h
@@ -1,24 +1,28 @@
 #pragma once
 #include <stdint.h>
 
+struct statem_entry_t
+{
+    void *ptr;
+    uint32_t duration;
+};
+
 typedef struct
 {
-    struct statem_entry_t
-    {
-        void *ptr;
-        uint32_t duration;
-    } *entries, current;
-
+    struct statem_entry_t *entries;
+    struct statem_entry_t; // ptr & duration
     uint16_t index;
     uint32_t timer;
+    uint16_t count;
 } statem_t;
 
 #define STATEM_TABLE(name, ...) \
     struct statem_entry_t name[] = {__VA_ARGS__}
 
-void statem_init(statem_t *, struct statem_entry_t *);
+void *statem_init(statem_t *, struct statem_entry_t *);
 uint16_t statem_tick(statem_t *);
 void *statem_next(statem_t *);
-void statem_setDuration(statem_t *, uint32_t);
-uint32_t statem_getDuration(statem_t *);
+void *statem_prev(statem_t *);
+void *statem_goto(statem_t *, uint16_t);
 uint16_t statem_getProgress(statem_t *);
+void *statem_reset(statem_t *);
