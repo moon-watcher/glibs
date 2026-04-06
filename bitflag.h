8753a68ccc95a70f2ef4b8f65e326d5e76570469
Moon-watcher <->
2026-04-07 01:50:16 +0200

    no message

--- bitflag.h
+++ bitflag.h
@@ -1,6 +1,6 @@
 #pragma once
 
-inline void     bitflag_set  (unsigned *flags, unsigned flag) {        *flags |=  (1U << flag); }
-inline unsigned bitflag_get  (unsigned *flags, unsigned flag) { return *flags &   (1U << flag); }
-inline void     bitflag_clear(unsigned *flags, unsigned flag) {        *flags &= ~(1U << flag); }
-inline void     bitflag_flip (unsigned *flags, unsigned flag) {        *flags ^=  (1U << flag); }
+#define bitflag_set(flags, flag)   ((*flags) |=  (1U << (flag)))
+#define bitflag_get(flags, flag)   ((*flags) &   (1U << (flag)))
+#define bitflag_clear(flags, flag) ((*flags) &= ~(1U << (flag)))
+#define bitflag_flip(flags, flag)  ((*flags) ^=  (1U << (flag)))
